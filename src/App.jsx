6f03cf5e0640e633dbef9e638fd76f00009f8f52
Felix Leclerc Jr <felixLJR@icloud.com>
2023-05-23 13:44:28 -0400

    Cleaned and ready for dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './index.css'
 
 function App() {
@@ -8,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1 className='test'>Hello world!</h1>
+
     </>
   )
 }
