3d21bc8399c413198ccfaaa82520034c7b1adb69
Felix Leclerc Jr <felixLJR@icloud.com>
2023-05-27 16:16:26 -0400

    cypress default setup

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,7 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+/// <reference types="cypress" />
+
+describe('Home Page', () => {
+  it('Should have two options in navbar', () => {
+    cy.visit('http://127.0.0.1:5173/')
   })
 })
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './index.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
 
   return (
     <>
+    <Navbar />
 
     </>
   )
