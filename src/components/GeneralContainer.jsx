d1d1b29050bbc73393d36bde0336c28137659794
Felix Leclerc Jr <felixLJR@icloud.com>
2023-05-28 14:19:54 -0400

    dropdown done

--- src/components/GeneralContainer.jsx
+++ src/components/GeneralContainer.jsx
@@ -1,6 +1,6 @@
 export default function Conatiner({children}) {
   return (
-    <div className='mx-auto w-[90%] rounded-lg h-fit bg-white px-6 py-4 sm:px-6 lg:px-8 sm:w-[50%]'>
+    <div className='mx-auto w-[90%] rounded-lg h-fit bg-white px-6 py-4 flex flex-col justify-center sm:px-6 lg:px-8 sm:w-[50%]'>
       {children}
     </div>
   )
